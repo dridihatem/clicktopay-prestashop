36310fcf28fd7f64bebaa91c7313be1bbaef72dc
Dridi Hatem <dridihatem@gmail.com>
2025-10-20 14:06:33 +0100

    feat: PrestaShop 8.2 compatibility\n- Update module metadata and versions\n- Secure SMT callback dispatch\n- Modernize payment options\n- Clean redirects for success/failure

---
+++ controllers/front/smtcontrol.php
@@ -0,0 +1,109 @@
+<?php
+
+
+/*
+ * (c) 2017 ExtrumWeb International <info@extrumweb.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class ClictopaySmtcontrolModuleFrontController extends ModuleFrontController
+{
+    public $Reference;
+    public $Action;
+    public $Params;
+    public $Montant;
+    public $Cart;
+
+    public function initContent()
+    {
+        parent::initContent();
+        $referenceRaw = Tools::getValue('Reference');
+        $actionRaw = Tools::getValue('Action');
+        $paramRaw = Tools::getValue('Param');
+
+        if (empty($referenceRaw) || empty($actionRaw)) {
+            die('');
+        }
+
+        $this->Reference = (int) $referenceRaw;
+        $this->Action = Tools::strtoupper(trim($actionRaw));
+        $this->Params = $paramRaw ? Tools::safeOutput($paramRaw) : null;
+
+        if ($this->Reference <= 0) {
+            die('');
+        }
+
+        $this->Cart = new Cart((int)$this->Reference);
+        if (!Validate::isLoadedObject($this->Cart)) {
+            die('');
+        }
+        $this->Montant = sprintf("%.3f", (float) $this->Cart->getOrderTotal());
+
+        $allowedActions = array(
+            'DETAIL' => 'DETAIL',
+            'ACCORD' => 'ACCORD',
+            'ERREUR' => 'ERREUR',
+            'REFUS' => 'REFUS',
+            'ANNULATION' => 'ANNULATION',
+        );
+
+        if (!isset($allowedActions[$this->Action])) {
+            die('');
+        }
+
+        $method = $allowedActions[$this->Action];
+        $resp = $this->$method();
+        if ($resp === false) {
+            die('');
+        }
+
+        die($resp);
+    }
+
+
+    /**
+     * Detail of order required by SMT
+     */
+    public function DETAIL()
+    {
+        return "Reference=" . $this->Cart->id . "&Action=" . $this->Action . "&Reponse=" . $this->Montant;
+    }
+
+    /**
+     * If payment accepted by SMT, Validate the Purchase Order,
+     */
+    public function ACCORD()
+    {
+        $this->module->setOrder(Configuration::get('SMT_OS_ACCEPTED'), $this->Cart, $this->Params);
+        return "Reference=" . $this->Cart->id . "&Action=" . $this->Action . "&Reponse=OK";
+    }
+
+    /**
+     * If a transaction error received by the SMT, Update the Purchase Order.
+     */
+    public function ERREUR()
+    {
+        $this->module->setOrder(Configuration::get('SMT_OS_ERROR'), $this->Cart);
+        return $response = "Reference=" . $this->Cart->id . "&Action=" . $this->Action . "&Reponse=OK";
+    }
+
+    /**
+     * If a refusal of payment received by the SMT, Update the Purchase Order.
+     */
+    public function REFUS()
+    {
+        $this->module->setOrder(Configuration::get('SMT_OS_REFUSED'), $this->Cart);
+        return "Reference=" . $this->Cart->id . "&Action=" . $this->Action . "&Reponse=OK";
+    }
+
+    /**
+     * If a cancellation of payment received by the SMT, Update the Purchase Order.
+     */
+    public function ANNULATION()
+    {
+        $this->module->setOrder(Configuration::get('SMT_OS_CANCELED'), $this->Cart);
+        return "Reference=" . $this->Cart->id . "&Action=" . $this->Action . "&Reponse=OK";
+    }
+}
