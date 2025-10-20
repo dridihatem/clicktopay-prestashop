48481860c7f1b2f577ba06cb63a4abbe61e6e124
Dridi Hatem <dridihatem@gmail.com>
2025-10-20 14:22:12 +0100

    Update copyright year and company name in success.php

--- controllers/front/success.php
+++ controllers/front/success.php
@@ -2,7 +2,7 @@
 
 
 /*
- * (c) 2017 ExtrumWeb International <info@extrumweb.com>
+ * (c) 2025 DA WEB COMPANY
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
